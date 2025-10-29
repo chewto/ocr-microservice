28c22cb6b60aafbe34fe28b5e421aaa0ec40b0f3
Jesus Lozada <jesuselozada@gmail.com>
2025-10-29 17:30:09 -0400

    added eventlet

---
+++ gunicorn_config.py
@@ -0,0 +1,20 @@
+import multiprocessing
+workers = 2 * multiprocessing.cpu_count() + 1
+# workers = 18
+worker_class = 'eventlet'  # Use gevent async workers
+worker_connections = 2000  # Maximum concurrent connections per worker
+timeout = 30
+graceful_timeout = 30
+keepalive = 2
+
+max_requests = 1000
+max_requests_jitter = 50
+
+...
+accesslog = "/Logs/ocr/gunicorn_access.log"  # Log HTTP requests to a file
+errorlog = "/Logs/ocr/gunicorn_error.log"  # Log errors to a file
+loglevel = "info"  # Set log verbosity (debug, info, warning, error, critical)
+
+
+# workers = 3
+bind = "0.0.0.0:4000"
